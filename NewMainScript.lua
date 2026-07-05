dcccdb7c4cf7aab87bbc629246f3e5275d95cbe6
sessioncodes <tyysaf27@gmail.com>
2026-07-04 17:00:50 -0700

    Update NewMainScript.lua

--- NewMainScript.lua
+++ NewMainScript.lua
@@ -11,10 +11,10 @@ end
 local function downloadFile(path, func)
 	if not isfile(path) then
 		local suc, res = pcall(function()
-			return game:HttpGet('https://raw.githubusercontent.com/sessioncodes/capevapecompiled/'..readfile('capevape/profiles/commit.txt')..'/'..select(1, path:gsub('capevape/', '')), true)
+			return game:HttpGet('https://raw.githubusercontent.com/sessioncodes/cape-v4/main/'..select(1, path:gsub('capevape/', '')), true)
 		end)
 		if not suc or res == '404: Not Found' then
-			error(res)
+			error(res or 'Failed to download file: '..path)
 		end
 		if path:find('.lua') then
 			res = '--This watermark is used to delete the file if its cached, remove it to make the file persist after vape updates.\n'..res
@@ -24,36 +24,10 @@ local function downloadFile(path, func)
 	return (func or readfile)(path)
 end
 
-local function wipeFolder(path)
-	if not isfolder(path) then return end
-	for _, file in listfiles(path) do
-		if file:find('loader') then continue end
-		if isfile(file) and select(1, readfile(file):find('--This watermark is used to delete the file if its cached, remove it to make the file persist after vape updates.')) == 1 then
-			delfile(file)
-		end
-	end
-end
-
 for _, folder in {'capevape', 'capevape/games', 'capevape/profiles', 'capevape/assets', 'capevape/libraries', 'capevape/guis'} do
 	if not isfolder(folder) then
 		makefolder(folder)
 	end
-end
-
-if not shared.VapeDeveloper then
-	local _, subbed = pcall(function()
-		return game:HttpGet('https://github.com/sessioncodes/capevapecompiled')
-	end)
-	local commit = subbed:find('currentOid')
-	commit = commit and subbed:sub(commit + 13, commit + 52) or nil
-	commit = commit and #commit == 40 and commit or 'main'
-	if commit == 'main' or (isfile('capevape/profiles/commit.txt') and readfile('capevape/profiles/commit.txt') or '') ~= commit then
-		wipeFolder('capevape')
-		wipeFolder('capevape/games')
-		wipeFolder('capevape/guis')
-		wipeFolder('capevape/libraries')
-	end
-	writefile('capevape/profiles/commit.txt', commit)
 end
 
 return loadstring(downloadFile('capevape/main.lua'), 'main')()
