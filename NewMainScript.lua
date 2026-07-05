3eded80ff56dcafc856625218c4965fb269e51f4
sessioncodes <tyysaf27@gmail.com>
2026-07-04 17:23:59 -0700

    Update NewMainScript.lua

--- NewMainScript.lua
+++ NewMainScript.lua
@@ -11,7 +11,7 @@ end
 local function downloadFile(path, func)
 	if not isfile(path) then
 		local suc, res = pcall(function()
-			return game:HttpGet('https://raw.githubusercontent.com/sessioncodes/cape-v4/'..select(1, path:gsub('capevape/', '')), true)
+			return game:HttpGet('https://raw.githubusercontent.com/sessioncodes/cape-v4/main/'..path, true)
 		end)
 		if not suc or res == '404: Not Found' then
 			error(res or 'Failed to download: '..path)
@@ -24,25 +24,17 @@ local function downloadFile(path, func)
 	return (func or readfile)(path)
 end
 
-local function wipeFolder(path)
-	if not isfolder(path) then return end
-	for _, file in listfiles(path) do
-		if file:find('loader') then continue end
-		if isfile(file) and select(1, readfile(file):find('--This watermark is used to delete the file if its cached, remove it to make the file persist after vape updates.')) == 1 then
-			delfile(file)
-		end
-	end
-end
-
+-- Create folders
 for _, folder in {'capevape', 'capevape/games', 'capevape/profiles', 'capevape/assets', 'capevape/libraries', 'capevape/guis'} do
 	if not isfolder(folder) then
 		makefolder(folder)
 	end
 end
 
--- Write commit.txt if it doesn't exist (bypass capevapecompiled dependency)
+-- Write commit.txt
 if not isfile('capevape/profiles/commit.txt') then
 	writefile('capevape/profiles/commit.txt', 'main')
 end
 
-return loadstring(downloadFile('capevape/main.lua'), 'main')()
+-- Download and run main script
+return loadstring(downloadFile('NewMainScript.lua'), 'NewMainScript')()
