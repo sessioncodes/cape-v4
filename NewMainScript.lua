9936bd4bff59d0e7422e52531d8d847e491c2bbb
sessioncodes <tyysaf27@gmail.com>
2026-07-04 17:18:11 -0700

    Update NewMainScript.lua

--- NewMainScript.lua
+++ NewMainScript.lua
@@ -11,7 +11,7 @@ end
 local function downloadFile(path, func)
 	if not isfile(path) then
 		local suc, res = pcall(function()
-			return game:HttpGet('https://raw.githubusercontent.com/sessioncodes/cape-v4/main/'..select(1, path:gsub('capevape/', '')), true)
+			return game:HttpGet('https://raw.githubusercontent.com/sessioncodes/cape-v4/'..select(1, path:gsub('capevape/', '')), true)
 		end)
 		if not suc or res == '404: Not Found' then
 			error(res or 'Failed to download: '..path)
